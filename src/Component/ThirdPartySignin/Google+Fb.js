e5bdf045763a45222b48de29f08bacc98bc21de3
Sunil Gorasiya <sunil.gorasiya@bacancytechnology.com>
2020-03-03 18:14:23 +0530

    Facebook Changes

--- src/Component/ThirdPartySignin/Google+Fb.js
+++ src/Component/ThirdPartySignin/Google+Fb.js
@@ -1,11 +1,12 @@
-import React, {Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import GOOGLEICON from 'react-native-vector-icons/AntDesign';
 import AppStyle from '../../assets/config/Styles';
-import {GoogleSignin, statusCodes} from 'react-native-google-signin';
+import { GoogleSignin, statusCodes } from 'react-native-google-signin';
 import conf from '../../assets/config/Googleconfig';
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
 
 class FBWithGSignin extends Component {
   signIn = async () => {
@@ -30,6 +31,59 @@ class FBWithGSignin extends Component {
     }
   };
 
+  Login = () => {
+
+    // if(Login)
+    let res = LoginManager.logOut();
+    // LoginManager.getDefaultAudience(user)
+    console.log('Logout res :', res);
+    // .then(res => console.log('Logout  res : ', res));
+    Platform.OS = 'android' ? LoginManager.setLoginBehavior('web_only') : null;
+    // Attempt a login using the Facebook login dialog asking for default permissions.
+    LoginManager.logInWithPermissions(['public_profile', 'email']).then(
+      function (result) {
+        if (result.isCancelled) {
+          console.log('Login cancelled');
+        } else {
+          AccessToken.getCurrentAccessToken().then(async data => {
+            console.log(data);
+            const infoRequest = new GraphRequest(
+              '/me?fields=name,email,picture.type(large)',
+              null,
+              (error, result) => {
+                if (error) {
+                  console.log('Error fetching data: ', error);
+                } else {
+                  alert(JSON.stringify(result));
+                  console.log(
+                    'Success fetching data: ',
+                    JSON.stringify(result),
+                  );
+                }
+              },
+            );
+
+            new GraphRequestManager().addRequest(infoRequest).start();
+
+            //second way
+            // await fetch(
+            //   'https://graph.facebook.com/v2.5/me?fields=id,first_name,last_name,email,picture.type(large)&access_token=' +
+            //     data.AccessToken,
+            // ).then(res => {
+            //   user = res.json();
+            //   console.log('user : ', user);
+            //   alert('Login Sucessfully');
+            // });
+          });
+        }
+      },
+      function (error) {
+        console.log('Login fail with error: ', error);
+      },
+    );
+  };
+
+
   componentDidMount() {
     GoogleSignin.configure(conf);
   }
@@ -40,9 +94,9 @@ class FBWithGSignin extends Component {
         <View style={styles.container}>
           <TouchableOpacity
             style={[styles.btn, styles.facebook_btn]}
-            onPress={() => {
-              alert('button pressed');
-            }}>
+            onPress={
+              this.Login
+            }>
             <Icon name="facebook" size={18} style={styles.Icon} />
             <Text style={[styles.Font, styles.fbFont]}>FACEBOOK</Text>
           </TouchableOpacity>
