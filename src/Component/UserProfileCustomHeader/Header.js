28721298808b5079793505eca3db6f2022ab012b
Sunil Gorasiya <sunil.gorasiya@bacancytechnology.com>
2020-03-04 15:56:40 +0530

    UserProfile Header

--- src/Component/UserProfileCustomHeader/Header.js
+++ src/Component/UserProfileCustomHeader/Header.js
@@ -1,25 +1,22 @@
-import React, {Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import ExitIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import styles from './Styles_Header';
 
 const Header = props => {
   return (
-    // new changes on version
     <View style={styles.Container}>
-      <View style={{alignSelf: 'flex-start'}}>
+      <View style={styles.iconStyle}>
         <ExitIcon name="exit-run" color={'black'} size={30} />
       </View>
-      <View style={styles.APP_BAR}>
-        <Text style={styles.App_NAME}>APP NAME</Text>
-
-        {/* Tab Bar Logic use here plz add Title here  */}
-
-        {/* Tab Bar End here */}
+      <View style={styles.titleStyle}>
+        <Text style={styles.txtStyle}>APP NAME</Text>
       </View>
     </View>
   );
 };
 
 export default Header;
+{/* 
+ */}
